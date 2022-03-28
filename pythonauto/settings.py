c46a3c971ef9e8bc2fb744c365a69b6448227626
George M <gmeyer1000@yahoo.com>
2022-03-27 22:11:31 -0700

    dbase

--- pythonauto/settings.py
+++ pythonauto/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from curses import def_prog_mode
 from pathlib import Path
 import os
 #import environ
@@ -87,13 +88,16 @@ WSGI_APPLICATION = 'pythonauto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default' : dj_database_url.config('DATABASE_URL', default=default_dburl)
 }
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
